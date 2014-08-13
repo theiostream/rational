a9cd490ae13686d33c7a560b63b9a0490a07f42b
Daniel Ferreira (theiostream) <daniel.calibeta@gmail.com>
2014-08-13 16:27:40 -0300

    First commit.

---
+++ Rational/src/Displayer.java
@@ -0,0 +1,20 @@
+import game.Rational;
+
+import java.io.File;
+
+import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+
+public class Displayer {
+	public static void main(String[] argv) {
+		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+		
+		try {
+			AppGameContainer container = new AppGameContainer(new Rational("Rational"));
+			container.setDisplayMode(Rational.getWidth(), Rational.getHeight(), false);
+			container.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Rational/src/game/Rational.java
@@ -0,0 +1,65 @@
+package game;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+
+import font.Fonts;
+
+public class Rational extends BasicGame {
+	public static int getWidth() { return 800; }
+	public static int getHeight() { return 600; }
+	
+	public Rational(String title) {
+		super(title);
+	}
+
+	private TrueTypeFont gameFont;
+	private int playerX=250, playerMove=0;
+	
+	public void init(GameContainer container) throws SlickException {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, getWidth(), getHeight(), 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		
+		try {
+			gameFont = Fonts.gameFont();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update(GameContainer container, int delta) throws SlickException {
+		playerX += playerMove;
+	}
+	
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		Box.MakeBox(g, "What? Where am I? Why is everything black?", gameFont);
+		
+		g.setColor(Color.green);
+		g.fillRect(playerX, 500, 50, 50);
+	}
+	
+	public void keyPressed(int key, char c) {
+		if (key == Keyboard.KEY_RIGHT) {
+			playerMove = 1;
+		}
+		else if (key == Keyboard.KEY_LEFT) {
+			playerMove = -1;
+		}
+	}
+	
+	public void keyReleased(int key, char c) {
+		playerMove = 0;
+	}
+}
